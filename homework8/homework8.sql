a2bc8e18b1303ec77f47df4f618bbb9922f77730
YAREN ÇELİK <91730447+celikyarent@users.noreply.github.com>
2022-04-14 15:00:58 +0300

    Update homework8.sql

--- homework8/homework8.sql
+++ homework8/homework8.sql
@@ -2,7 +2,7 @@
 
 
 										
-										-- Merhabalar,
+-- Merhabalar,
  
 --SORU1 test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
 				    CREATE table employee (
